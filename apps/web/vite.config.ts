624d8a9937d9cc5a0b6a866e60357984ee25c57a
Douglas Winter <a.winterdoug@gmail.com>
2023-04-20 21:12:47 +0200

    Removing rollup vite from rollup bundle

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,4 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   cacheDir: ".vite",
+  build: {
+    rollupOptions: {
+      // https://styled-components.com/docs/faqs#removing-styledcomponents-from-your-library-bundle
+      external: ["styled-components"],
+    },
+  },
 });
