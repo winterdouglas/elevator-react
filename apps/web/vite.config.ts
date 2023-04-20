af8a47fd05b65c4a788310345c8a5261006ef883
Douglas Winter <a.winterdoug@gmail.com>
2023-04-20 22:54:04 +0200

    Changing vite base url

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
   cacheDir: ".vite",
-  base: "/elevator-react-native",
+  base: "/elevator-react",
 });
